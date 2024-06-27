c896e9b6bbb2005878b44e418ca63216eaf48c9c
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-06-27 18:44:15 +0900

    Updated base.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import express from "express";
 const app = express();
 
 app.get('/', (req,res) => {
-    const headers = req.headers;
-
-    res.status(200).send(headers);
+    res.status(200).send("Hi there!");
 });
 
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.EXPRESS_PORT || 3000;
 app.listen(port, () => {
-    console.log(`Express listening on port ${process.env.SERVER_PORT}`);
+    console.log(`Express listening on port ${port}`);
 });
